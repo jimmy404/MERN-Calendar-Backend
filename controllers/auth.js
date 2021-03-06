4d7dfb0a895fc80b5acf6caf240dba44e5c0eb15
Jimmy200 <bruno_pier@hotmail.com>
2021-03-06 13:58:34 -0300

    Adds req.body data and first validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,19 +1,37 @@
+const { response } = require('express');
 
-const crearUsuario = (req, res) => {
+const crearUsuario = (req, res = response) => {
+
+  const { name, email, password } = req.body;
+
+  if( name.length < 5 ) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'El nombre debe ser de 5 letras'
+    });
+  }
+
+  return res.json({
+    ok: true,
+    msg: 'Registro',
+    name,
+    email,
+    password,
+  });
+};
+
+const loginUsuario = (req, res = response) => {
+  const { email, password } = req.body;
+
   res.json({
     ok: true,
-    msg: 'Registro'
+    msg: 'Login',
+    email,
+    password,
   })
 };
 
-const loginUsuario = (req, res) => {
-  res.json({
-    ok: true,
-    msg: 'Login'
-  })
-};
-
-const revalidarToken = (req, res) => {
+const revalidarToken = (req, res = response) => {
   res.json({
     ok: true,
     msg: 'Renew'
