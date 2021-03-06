611c37e54faa2ff24b030e430a10ccfd854df504
Jimmy200 <bruno_pier@hotmail.com>
2021-03-06 16:04:47 -0300

    Deleted old validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,8 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
 
   const { name, email, password } = req.body;
-
-  //Manejo de errores
-  const errors = validationResult(req);
-  if( !errors.isEmpty() ) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    });
-  }
 
   return res.status(201).json({
     ok: true,
@@ -24,15 +14,6 @@ const crearUsuario = (req, res = response) => {
 };
 
 const loginUsuario = (req, res = response) => {
-
-  //Manejo de errores
-  const errors = validationResult(req);
-  if( !errors.isEmpty() ) {
-    return res.status(400).json({
-      ok: false,
-      errors: errors.mapped()
-    });
-  }
 
   const { email, password } = req.body;
 
