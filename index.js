2995ba4b67f923c5078dc59d9a89fdcc9909d1ef
Jimmy200 <bruno_pier@hotmail.com>
2021-03-06 12:02:20 -0300

    First test on express

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log('Testing nodemon');
+const express = require('express');
+
+// Crear servidor express
+const app = express();
+
+// Rutas
+app.get('/', (req, res) => {
+  console.log('Se require el /');
+  res.json({
+    ok: true
+  })
+});
+
+// Escuchar peticiones
+app.listen(4000, () => {
+  console.log(`Servidor corriendo en puerto ${4000}`)
+});
+
+
