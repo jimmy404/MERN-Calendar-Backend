02a8781d678451c78adaff1d69a7965758d5a201
Jimmy200 <bruno_pier@hotmail.com>
2021-03-06 16:05:17 -0300

    Adds middleware for validations in routes

--- routes/auth.js
+++ routes/auth.js
@@ -5,9 +5,11 @@
 
 const { Router } = require('express');
 const { check } = require('express-validator')
+const { validarCampos } = require('../middlewares/validar-campos');
+const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
+
 const router = Router();
 
-const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
 router.post(
   '/new',
@@ -16,6 +18,7 @@ router.post(
     check('name', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+    validarCampos,
   ],
   crearUsuario
 );
@@ -26,6 +29,7 @@ router.post(
     // Middlewares
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+    validarCampos,
   ],
   loginUsuario
 );
