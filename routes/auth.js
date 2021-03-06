2987e1a4d0b3bf5cbf62c484eeb50c54eceb35f0
Jimmy200 <bruno_pier@hotmail.com>
2021-03-06 14:18:52 -0300

    Adds validations for errors

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,31 @@
 */
 
 const { Router } = require('express');
+const { check } = require('express-validator')
 const router = Router();
 
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
 
-router.post('/new', crearUsuario);
+router.post(
+  '/new',
+  [
+    // Middlewares
+    check('name', 'El nombre es obligatorio').not().isEmpty(),
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+  ],
+  crearUsuario
+);
 
-router.post('/', loginUsuario);
+router.post(
+  '/',
+  [
+    // Middlewares
+    check('email', 'El email es obligatorio').isEmail(),
+    check('password', 'El password debe de ser de 6 caracteres').isLength({min: 6}),
+  ],
+  loginUsuario
+);
 
 router.get('/renew', revalidarToken);
 
