ffc5875336ddb18a304a2a4fb81c108d27ba7796
Jimmy200 <bruno_pier@hotmail.com>
2021-03-06 14:19:34 -0300

    Adds validations for login and create user

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,17 +1,20 @@
 const { response } = require('express');
+const { validationResult } = require('express-validator');
 
 const crearUsuario = (req, res = response) => {
 
   const { name, email, password } = req.body;
 
-  if( name.length < 5 ) {
+  //Manejo de errores
+  const errors = validationResult(req);
+  if( !errors.isEmpty() ) {
     return res.status(400).json({
       ok: false,
-      msg: 'El nombre debe ser de 5 letras'
+      errors: errors.mapped()
     });
   }
 
-  return res.json({
+  return res.status(201).json({
     ok: true,
     msg: 'Registro',
     name,
@@ -21,6 +24,16 @@ const crearUsuario = (req, res = response) => {
 };
 
 const loginUsuario = (req, res = response) => {
+
+  //Manejo de errores
+  const errors = validationResult(req);
+  if( !errors.isEmpty() ) {
+    return res.status(400).json({
+      ok: false,
+      errors: errors.mapped()
+    });
+  }
+
   const { email, password } = req.body;
 
   res.json({
