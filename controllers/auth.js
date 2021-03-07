0f39f8d38fa5b8500198921464815507d29964e4
Jimmy200 <bruno_pier@hotmail.com>
2021-03-06 23:55:55 -0300

    Adds JWT for auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const { response } = require('express');
 const bcrypt = require('bcryptjs');
 const Usuario = require('../models/Usuario');
+const { generarJWT } = require('../helpers/jwt');
 
 const crearUsuario = async (req, res = response) => {
 
@@ -22,10 +23,14 @@ const crearUsuario = async (req, res = response) => {
 
     await usuario.save();
 
+    // Generar JWT
+    const token = await generarJWT(usuario.id, usuario.name);
+
     return res.status(201).json({
       ok: true,
       uid: usuario.id,
-      name: usuario.name
+      name: usuario.name,
+      token
     });
   } catch (error) {
     console.log(error)
@@ -61,11 +66,13 @@ const loginUsuario = async (req, res = response) => {
     }
 
     // Generar JWT
+    const token = await generarJWT(usuario.id, usuario.name);
+
     res.json({
       ok: true,
       uid: usuario.id,
-      name: usuario.name
-
+      name: usuario.name,
+      token
     })
 
   } catch (error) {
